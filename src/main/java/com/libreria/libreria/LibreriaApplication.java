7c1a12d4d2bed1773d00cfe76a67ec7cdb86272f
joga17000 <joga17000@gmail.com>
2025-12-17 13:50:33 -0600

    feat: Initialize Spring Boot application with dotenv integration for environment variable loading and basic database configuration.

--- src/main/java/com/libreria/libreria/LibreriaApplication.java
+++ src/main/java/com/libreria/libreria/LibreriaApplication.java
@@ -1,5 +1,6 @@
 package com.libreria.libreria;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibreriaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
 
